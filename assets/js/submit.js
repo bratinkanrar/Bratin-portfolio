08a73643c8aff2692ae08c7dbec68fbef3b07a60
BRATIN KANRAR <59428554+bratinkanrar@users.noreply.github.com>
2025-01-14 17:44:15 +0530

    Update submit.js

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -3,7 +3,7 @@ f.addEventListener("submit", s => {
     s.preventDefault();
     let i = new FormData(f)
       , n = Object.fromEntries(i.entries());
-    var e = "https://wa.me/916291640712?text=Name: " + n.name + "%0aE-mail: " + n.email + "%0aMessage: " + n.message;
+    var e = "https://wa.me/916291640712?text=Name: " + n.fullname + "%0aE-mail: " + n.email + "%0aMessage: " + n.message;
     window.open(e, "_blank").focus()
 }
 );
